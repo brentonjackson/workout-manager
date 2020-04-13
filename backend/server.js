907b6935abdef078dda0012f675eeae610b3b7a5
Brentonius <bjavonjackson@gmail.com>
2020-04-13 16:14:27 -0400

    added and implemented server and api endpoints to create, read, and update items

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,80 @@ const cors = require('cors');
 
 // add mongodb
 const mongoose = require('mongoose');
+let Workout = require('./workout.model');
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://124.0.0.1:27017/workouts', { useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/workouts', { useNewUrlParser: true});
 const connection = mongoose.connection;
 
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
+
+
+
+//****************************************************** setup api endpoints *************************************************************************
+const workoutRoutes = express.Router();
+
+
+// endpoint to deliver all available workout items
+// get workouts when result is available using find method
+workoutRoutes.route('/').get((req,res) => {
+	Workout.find((err, workouts) => {
+		if (err) {
+			console.log(err + 'man');
+		} else {
+			res.json(workouts);
+		}
+	});
+});
+
+// endpoint to retrieve workout item
+workoutRoutes.route('/:id').get((req, res) => {
+	let id = req.params.id;
+	Workout.findById(id, (err, workout) => {
+		res.json(workout);
+	});
+});
+
+// Post endpoint route
+workoutRoutes.route('/add').post((req, res) => {
+	let workout = new Workout(req.body);
+	workout.save()
+		.then(workout => {
+			res.status(200).json({'workout': 'workout added successfully'});
+		})
+		.catch(err => {
+			res.status(400).send('adding new workout failed');
+		});
+});
+
+// update endpoint route
+workoutRoutes.route('/update/:id').post((req, res) => {
+	Workout.findById(req.params.id, (err, workout) => {
+		if (!workout) {
+			res.status(404).send('data is not found');
+		} else {
+			workout.workout_description = req.body.workout_description;
+			workout.workout_responsible = req.body.workout_responsible;
+			workout.workout_difficulty = req.body.workout_difficulty;
+			workout.workout_times_completed = req.body.workout_times_completed;
+			workout.workout_completed_date = req.body.workout_completed_date;
+
+			workout.save().then(workout => {
+				res.json('Workout updated!');
+			})
+			.catch(err => {
+				res.status(400).send('Update not possible');
+			});
+		}
+	})
+})
+
+app.use('/workouts', workoutRoutes);
 
 app.listen(PORT, function() {
     console.log(`Server is running on Port: ${PORT}`);
--- backend/workout.model.js
+++ backend/workout.model.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let Todo  = new Schema({
+let Workout  = new Schema({
     workout_description: {
         type: String
     },
+    workout_responsible: {
+    	type: String
+    },
+    workout_difficulty: {
+    	type: String
+    },
+    workout_times_completed: {
+    	type: Number
+    },
+    workout_completed_date: {
+    	type: Date, default: Date.now
+    }
+});
 
-})
+module.exports = mongoose.model('Workout', Workout);
