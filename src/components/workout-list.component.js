b6ce3d86f79625eda22e4e698cae89cfdf118ff5
Brentonius <bjavonjackson@gmail.com>
2021-01-05 19:56:58 -0500

    console log the workouts on mount

--- src/components/workout-list.component.js
+++ src/components/workout-list.component.js
@@ -82,7 +82,7 @@ export default class WorkoutList extends Component {
   })
             .then(response => {
 				this.setState({ workouts: response.data });
-				console.log('workouts',workouts)
+				console.log('workouts', this.state.workouts)
             })
             .catch(function (error){
                 console.log(error);
